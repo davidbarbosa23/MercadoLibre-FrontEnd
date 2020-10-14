3772e0746028161c3e0a88ca49b71941bfee389c
David Barbosa <davidbarbosa.dev@gmail.com>
2020-10-13 21:28:10 -0500

    Home style

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="container jumbotron bg-white mt-3">
-      <h1 className="display-4">Mercado Libre</h1>
-      <p className="lead">
-        La comunidad de compra y venta online más grande de América Latina.
-      </p>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-lg-10 mx-auto">
+          <div className="jumbotron bg-white mt-3">
+            <h1 className="display-4">Mercado Libre</h1>
+            <p className="lead">
+              La comunidad de compra y venta online más grande de América
+              Latina.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
